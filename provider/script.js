6332d2f0086e234d0c7ff37c5b08d2ea1b721981
Tazuddin <Tazuddin@NEXT_IT.com>
2018-12-05 08:44:18 +0600

    more on providder

--- provider/script.js
+++ provider/script.js
@@ -8,7 +8,7 @@ let myApp = angular.module('app', [])
 
 
 
-vm.launchUnicorn = function(){
+vm.launchUnicorn = function(){    
     unicornLauncher.launch();
     vm.numberOfLaunch = unicornLauncher.numberOfLaunch;
 }
@@ -24,12 +24,62 @@ myApp.value('clientId', 'abc123jkl345')
     }
     return encrypt(clientId, secrete);
 })
-.service('unicornLauncher', UnicornLauncher);
+// .service('unicornLauncher', UnicornLauncher)
+;
 
-function UnicornLauncher(apiToken){
-    this.numberOfLaunch = 0;
+
+myApp.provider('unicornLauncher', function(){
+    var useTinfoilShielding = false;
+    var planetName = 'Earth';
+
+    this.useTinfoilShielding = function(value){
+        useTinfoilShielding = !!value;
+    }
+    this.launchFrom = function(from){        
+        planetName = from;
+    }
+
+    this.$get = function(apiToken){        
+        let obj = new UnicornLauncher(apiToken, useTinfoilShielding, planetName);       
+        return obj;
+    }
+});
+
+function UnicornLauncher(apiToken, useTinfoilShielding, planetName){    
+    this.numberOfLaunch = 0;  
     this.launch = function(){
+        if(useTinfoilShielding){
+            console.log('with tinfoil shielding');
+        }        
+        console.log('launching from : ', planetName);        
         // launch
         this.numberOfLaunch+=1;
     }    
 }
+
+myApp.config(function(unicornLauncherProvider, planetName){    
+    unicornLauncherProvider.useTinfoilShielding(true);
+    unicornLauncherProvider.launchFrom(planetName);
+});
+
+myApp.constant('planetName', 'Cremetoria');
+
+myApp.directive('planet', function(planetName){    
+    return {
+        restrict: 'E',
+        scope: {},
+        link: function($scope, $element){
+            $element.text('Planet Name : '+ planetName);
+        }
+    }
+});
+
+
+myApp.config(function($provide){
+    console.log('$provide:  ', $provide);
+});
+
+
+
+
+
